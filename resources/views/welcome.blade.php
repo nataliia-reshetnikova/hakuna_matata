57083ec6ec3544b95080e9191cc29f5d7cb466bc
Nata <ab1748@gmail.com>
2020-03-11 13:04:01 -0400

    added authentification

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,16 +85,12 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/">Start</a>
+                    <a href="https://github.com/nataliereshetnikova/hakuna_matata">GitHub</a>
                 </div>
+                
             </div>
         </div>
+        <footer class="flex-center">Made by Nataliia, Amaka, Hamidreza</footer>
     </body>
 </html>
