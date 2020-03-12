69f4211cb576288338ee5c713d260656f970a7f9
Nata <ab1748@gmail.com>
2020-03-12 13:46:57 -0400

    add username to user registration pattern, need to run php artisan migrate:fresh to refresh database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,8 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('email_verified_at')->nullable();//not required, can be null
+            $table->string('username')->unique();//should be unique
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
